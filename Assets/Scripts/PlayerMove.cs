66ae038437c90209a7dcebca1f16ee47e03601bf
mineko <ttc2550sg0023@edu.tech.ac.jp>
2025-12-04 10:01:13 +0900

    PlayerMovement Update

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,24 +3,92 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour
 {
-    public float MoveSpeed = 0.1f;
-    //プレイヤーの入力を受け取るための関数
-    private void OnMove(InputValue value)
+    [SerializeField] private float MoveSpeed = 3;
+    [SerializeField] private float JumpSpeed = 7;
+    [SerializeField] private float Gravity = 15;
+    [SerializeField] private float FallSpeed = 10;
+    [SerializeField] private float initFallSpeed = 2;
+    private Transform _transform;
+    private CharacterController _characterController;
+
+    private Vector2 _inputMove;
+    private float _verticalVelocity;
+    private float _turnVelocity;
+    private bool _isGroundedPrev;
+    public void OnMove(InputAction.CallbackContext context)
     {
-        //受け取った値をオブジェクトに渡す（オブジェクトの移動）
-        Vector2 axis = value.Get<Vector2>();
+        _inputMove = context.ReadValue<Vector2>();
+    }
 
-        //自身のPositionを変数に保持（現在位置の記録）
-        Vector3 PlayerVector = this.transform.position;
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        // ボタンが押された瞬間かつ着地している時だけ処理
+        if (!context.performed || !_characterController.isGrounded) return;
 
-        Vector3 updatePlayerPosition = new Vector3(
-           PlayerVector.x + axis.x * MoveSpeed,
-           PlayerVector.y,
-           PlayerVector.z + axis.y * MoveSpeed);
+        // 鉛直上向きに速度を与える
+        _verticalVelocity = JumpSpeed;
+    }
 
-        //記録した位置に入力された値を加算して新しい位置を計算
-        this.transform.position = updatePlayerPosition;
+    private void Awake()
+    {
+        _transform = transform;
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        var isGrounded = _characterController.isGrounded;
+
+        if (isGrounded && !_isGroundedPrev)
+        {
+            // 着地する瞬間に落下の初速を指定しておく
+            _verticalVelocity = -initFallSpeed;
+        }
+        else if (!isGrounded)
+        {
+            // 空中にいるときは、下向きに重力加速度を与えて落下させる
+            _verticalVelocity -= Gravity * Time.deltaTime;
+
+            // 落下する速さ以上にならないように補正
+            if (_verticalVelocity < -FallSpeed)
+                _verticalVelocity = -FallSpeed;
+        }
+
+        _isGroundedPrev = isGrounded;
+
+        // 操作入力と鉛直方向速度から、現在速度を計算
+        var moveVelocity = new Vector3(
+            _inputMove.x * MoveSpeed,
+            _verticalVelocity,
+            _inputMove.y * MoveSpeed
+        );
+        // 現在フレームの移動量を移動速度から計算
+        var moveDelta = moveVelocity * Time.deltaTime;
+
+        // CharacterControllerに移動量を指定し、オブジェクトを動かす
+        _characterController.Move(moveDelta);
+
+        if (_inputMove != Vector2.zero)
+        {
+            // 移動入力がある場合は、振り向き動作も行う
+
+            // 操作入力からy軸周りの目標角度[deg]を計算
+            var targetAngleY = -Mathf.Atan2(_inputMove.y, _inputMove.x)
+                * Mathf.Rad2Deg + 90;
+
+            // イージングしながら次の回転角度[deg]を計算
+            var angleY = Mathf.SmoothDampAngle(
+                _transform.eulerAngles.y,
+                targetAngleY,
+                ref _turnVelocity,
+                0.1f
+            );
+
+            // オブジェクトの回転を更新
+            _transform.rotation = Quaternion.Euler(0, angleY, 0);
+        }
     }
 }
